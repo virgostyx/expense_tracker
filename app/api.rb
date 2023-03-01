395405b6b8d2a3fffe9f445a921e048267e62f7f
virgostyx <virgostyx@gmail.com>
2023-03-01 07:32:36 +0000

    Initial commit

---
+++ app/api.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+  class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+  end
+end
